2b53d793e0326acbf0f2cbfe536df30079cda1e8
Nufais Ahamed <ahamednufais4161@gmail.com>
2025-03-26 10:45:32 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const productController = require("../controller/productController")
-const { verifyRole } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 
 router.get("/products",productController.getAllproducts);
-router.post("/products",verifyRole, productController.createProduct);
+router.post("/products",productController.createProduct);
 router.put("/products/:productId",productController.updateProduct);
 router.delete("/products/:productId",productController.deleteProduct);
 
